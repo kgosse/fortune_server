415f227de057b27c0fb70464569965ceca9b6d22
Valentino Houessou <valentino.houessou@gmail.com>
2017-01-02 03:48:09 +0100

    change project framework too loopback

---
+++ common/models/fortune.js
@@ -0,0 +1,78 @@
+'use strict';
+
+module.exports = function(Fortune) {
+  /*Call before a new instance of review is created*/
+    Fortune.beforeRemote('create', function(context, user, next) {
+    context.args.data.time = Date.now();
+    if(context.req.accessToken !== null)
+      context.args.data.ownerId = context.req.accessToken.userId;
+    next();
+  });
+  
+  //A static method you define    
+  Fortune.setLike = function(fortuneId, cb) {
+    Fortune.findById( fortuneId, function (err, instance) {
+        let nlike =  instance.like + 1;
+        instance.updateAttribute("like", nlike , function (err, instance) {
+          var response = "this fortune like was well processed new value is: " + instance.like;
+          cb(null, response);
+          console.log(response);
+        });
+    });
+  }
+  
+  //A static method you define    
+  Fortune.setDislike = function(fortuneId, cb) {
+    Fortune.findById( fortuneId, function (err, instance) {
+        let ndislike =  instance.dislike + 1;
+        instance.updateAttribute("dislike", ndislike , function (err, instance) {
+          var response = "this fortune dislike was well processed new value is: " + instance.dislike;
+          cb(null, response);
+          console.log(response);
+        });
+    });
+  }
+  
+  //A static method you define    
+  Fortune.setMessage = function(fortuneId, msg, cb) {
+    Fortune.findById( fortuneId, function (err, instance) {
+        instance.updateAttribute("message", msg, function (err, instance) {
+          var response = "the message of this fortune have benn well changed. The new message is : " + instance.message;
+          cb(null, response);
+          console.log(response);
+        });
+    });
+  }
+  
+  //remoteMethod for process a like
+  Fortune.remoteMethod (
+        'setLike',
+        {
+          http: {path: '/like', verb: 'put'},
+          accepts: {arg: 'id', type: 'any', http: { source: 'query' } },
+          returns: {arg: 'like', type: 'string'}
+        }
+    );
+  
+  //remoteMethod for process a like
+  Fortune.remoteMethod (
+        'setDislike',
+        {
+          http: {path: '/dislike', verb: 'put'},
+          accepts: {arg: 'id', type: 'any', http: { source: 'query' } },
+          returns: {arg: 'dislike', type: 'string'}
+        }
+    );
+  
+  //remoteMethod for process a like
+  Fortune.remoteMethod (
+        'setMessage',
+        {
+          http: {path: '/message', verb: 'put'},
+          accepts: [{arg: 'id', type: 'any', http: { source: 'query' } },
+                    {arg: 'msg', type: 'string', http: { source: 'query' } }],
+          returns: {arg: 'dislike', type: 'string'}
+        }
+    );
+  
+};
