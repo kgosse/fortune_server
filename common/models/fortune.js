55ff1de0bd91218f455af7c55e9a1ef02e7dd61f
Kévin Gossé <gossekevin1@gmail.com>
2017-01-07 11:41:13 +0100

    Fix like and dislike

--- common/models/fortune.js
+++ common/models/fortune.js
@@ -14,7 +14,7 @@ module.exports = function(Fortune) {
   /*operationnal hook call before retrieve data*/
   Fortune.observe('loaded', function(ctx, next){
     if(ctx.instance){
-      let total =  ctx.instance.like - ctx.instance.dislike;
+      let total =  (ctx.instance.like || 0) - (ctx.instance.dislike || 0);
       ctx.instance.total = total;
     }
     next();
@@ -23,7 +23,7 @@ module.exports = function(Fortune) {
   //A static method you define    
   Fortune.setLike = function(fortuneId, cb) {
     Fortune.findById( fortuneId, function (err, instance) {
-        let nlike = instance.like + 1;
+        let nlike = (instance.like || 0) + 1;
         instance.updateAttribute("like", nlike , function (err, instance) {
           var response = instance.like;
           cb(null, response);
@@ -47,7 +47,7 @@ module.exports = function(Fortune) {
   //A static method you define    
   Fortune.setDislike = function(fortuneId, cb) {
     Fortune.findById( fortuneId, function (err, instance) {
-        let ndislike = instance.dislike + 1;
+        let ndislike = (instance.dislike || 0) + 1;
         instance.updateAttribute("dislike", ndislike , function (err, instance) {
           var response = instance.dislike;
           cb(null, response);
